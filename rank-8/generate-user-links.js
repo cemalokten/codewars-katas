74c2f67aceaa4e276cf216bf9db5c2f4b0a3de70
Cemal Okten <cemalokten@gmakil.com>
2021-06-13 18:51:47 +0100

    Create generate-user-links.js
    Convert string into UTF8 link

---
+++ rank-8/generate-user-links.js
@@ -0,0 +1,16 @@
+// Kata 8
+// Title - Generate user links
+// Description
+
+// Your task is to create userlinks for the url, you will be given a username and must return a valid link.
+// Example
+
+// generate_link('matt c')
+// http://www.codewars.com/users/matt%20c
+
+
+function generateLink(user) {
+  return `http://www.codewars.com/users/${encodeURIComponent(user)}`
+}
+
+
