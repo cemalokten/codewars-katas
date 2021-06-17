ec6209a03ef6e602aa29d54ffc178629d34de644
Cemal Okten <cemalokten@gmakil.com>
2021-06-17 06:47:40 +0100

    Create highest-and-lowest.js
    Return highest and lowest values of an array

---
+++ rank-8/highest-and-lowest.js
@@ -0,0 +1,21 @@
+// Kata 7
+// Title - Highest and lowest
+// Description -
+// In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+
+// Example:
+
+// highAndLow("1 2 3 4 5");  // return "5 1"
+// highAndLow("1 2 -3 4 5"); // return "5 -3"
+// highAndLow("1 9 3 4 -5"); // return "9 -5"
+
+// Notes:
+
+//     All numbers are valid Int32, no need to validate them.
+//     There will always be at least one number in the input string.
+//     Output string must be two numbers separated by a single space, and highest number is first.
+
+function highAndLow(numbers) {
+	const arr = numbers.split(' ').map((c) => Number(c));
+	return String(`${Math.max(...arr)} ${Math.min(...arr)}`);
+}
