9532f036de16befc0a275e8b68f26003776037ba
cemalokten <cemalokten@gmail.com>
2021-07-09 17:21:05 +0100

    🛰

--- rank-5/string-incrementer.js
+++ rank-5/string-incrementer.js
@@ -1,9 +1,9 @@
 function incrementString(string) {
 	const arr = string.split(/([0-9]+)/);
 	if (arr[1] != undefined) {
-		const len = arr[1].length;
+		const length = arr[1].length;
 		arr[1] = Number(arr[1]) + 1;
-		return arr[0] + String(arr[1]).padStart(len, 0);
+		return arr[0] + String(arr[1]).padStart(length, 0);
 	} else {
 		return string + 1;
 	}
