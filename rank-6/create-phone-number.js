073fc93aeca5d4f26bd4413ebe8ad197e25b50db
cemalokten <cemalokten@gmail.com>
2021-07-11 16:12:11 +0100

    Rought solution (needs refining)

---
+++ rank-6/create-phone-number.js
@@ -0,0 +1,8 @@
+//Kata 6
+//Title: Create Phone Number
+
+//Not the best way of doing this, but it works...
+
+function createPhoneNumber(n) {
+	return `(${n[0]}${n[1]}${n[2]}) ${n[3]}${n[4]}${n[5]}-${n[6]}${n[7]}${n[8]}${n[9]}`;
+}
