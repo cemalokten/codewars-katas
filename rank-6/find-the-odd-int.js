47b64ea521b7100d92893b4036672be48e561fe1
cemalokten <cemalokten@gmail.com>
2021-07-25 14:21:37 +0100

    Solution 👏

--- rank-6/find-the-odd-int.js
+++ rank-6/find-the-odd-int.js
@@ -1,27 +1,28 @@
-function findOdd(A) {
-  
-  return 0;
-}
-console.log(findOdd([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]));  //6
-
-  var votes = [20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]
-
-var reducer = function(tally, vote) {
-  if (!tally[vote]) {
-    tally[vote] = 1;
+const reducer = function(obj, key) {
+  if (!obj[key]) {
+    obj[key] = 1;
   } else {
-    tally[vote] = tally[vote] + 1;
+    obj[key] = obj[key] + 1;
   }
-
-
-  return tally
+  return obj
 }
-var result = votes.reduce(reducer, {}) // {tacos: 2, pizza: 3, fries: 1, ice cream: 2}
 
-console.log(result);
+
+function findOdd(A) {
+  const result = A.reduce(reducer, {})
+    let num = 0;
 
 for (const [key, value] of Object.entries(result)) {
   if (value % 2 == 1) {
-      return key;
+      num = key;
   }
 }
+
+  return Number(num);
+}
+
+console.log(findOdd([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]));  //6
+
+
+
+
